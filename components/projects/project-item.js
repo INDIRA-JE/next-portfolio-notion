9464d7f96d3ed1d63978839ceabfa3a1ef5c03ef
JE <whddms87@gmail.com>
2023-04-04 21:24:59 +0900

    Project 화면 적용 - 이슈해결 : Import Image 경로추가(/legacy/)

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -1,14 +1,22 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 export default function ProjectItem({ data }) {
   const Title = data.properties.Name.title[0].plain_text;
   const github = data.properties.Github.url;
   const description = data.properties.Description.rich_text[0].plain_text;
-  const imgScr = data.cover.external.url;
+  const imgScr = data.cover.file?.url || data.cover.external.url;
 
   return (
-    <div className="flex flex-col p-6 m-3 bg-slate-700 rounded-md">
-      <Image src={imgScr} width="100" height="60" />
+    <div className="flex flex-col m-3 bg-slate-700 rounded-xl">
+      <Image
+        className="rouded-t-xl"
+        src={imgScr}
+        alt="cover image"
+        width="100%"
+        height="50%"
+        layout="responsive"
+        objectFit="none"
+      />
       <h1>{Title}</h1>
       <h3>{description}</h3>
       <a href={github}>Github 링크 바로가기</a>
